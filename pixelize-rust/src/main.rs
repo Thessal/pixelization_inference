691b7688dc15868c3c448e4bec215b8fd3a8536b
Jongkook Choi <jongkook90@gmail.com>
2023-02-01 14:47:06 +0900

    Added rust inference template

--- pixelize-rust/src/main.rs
+++ pixelize-rust/src/main.rs
@@ -1,11 +1,12 @@
-use std::env;
+use std::{env, error::Error, io::Cursor};
 
-use std::io::Cursor;
-use image::io::Reader as ImageReader;
+use image::{io::Reader as ImageReader, Rgb32FImage};
 
 use std::collections::HashMap;
 use onnxruntime::{
-    environment::Environment, ndarray::Array, tensor::OrtOwnedTensor, GraphOptimizationLevel,
+    environment::Environment, 
+    ndarray::{Array, Dim, Ix0, Ix1, Ix2, Ix3, Ix4, Ix5},
+    tensor::OrtOwnedTensor, GraphOptimizationLevel,
     LoggingLevel, OrtError
 };
 
@@ -43,21 +44,37 @@ fn load_model_all<'a>(environment:&'a Environment)-> HashMap<std::string::String
     let models = model_names
         .map(
             |i| (i.clone(), load_model(&environment, model_data[&i]).expect("Model Load Error"))
-            ).collect::<HashMap<String, onnxruntime::session::Session<'_>>>();
+            ).collect::<HashMap<String, onnxruntime::session::Session<'a>>>();
     models
 }
 
-fn process_image(models: &HashMap<String,onnxruntime::session::Session<'_>>, data: String){
-    println!("{}",data);
+fn process_image(models: &mut HashMap<String, onnxruntime::session::Session<'_>>, data: Array<f32, Ix4>){
+    //println!("{:?}",data);
+    // TODO : preprocess
+    let _model = models.get_mut(&("g_a_RGBEnc".to_string()));
+    let model = _model.unwrap();
+    let i = vec![data];
+    let outputs : Vec<OrtOwnedTensor<f32, _>> = model.run(i).unwrap();
+    println!("{:?}", outputs);
 }
 
-fn process_image_all(models: &HashMap<String,onnxruntime::session::Session<'_>>, image_path: Vec<String>){
-    for x in image_path{
-        //let img = ImageReader::open("myimage.png")?.decode()?;
-        let data = x.clone();
-        process_image(models, data);
+fn process_image_all(models: &mut HashMap<String, onnxruntime::session::Session<'_>>, image_path: Vec<String>) -> Result<(),()>{
+    let reference_b = Cursor::new(include_bytes!("../../reference.png"));
+    let reference_data = ImageReader::new(reference_b).with_guessed_format().unwrap().decode().unwrap();
+    for filename in image_path{
+        //let image = ImageReader::open("../input_file.jpg").expect("File open failed")
+        let image = ImageReader::open("../red.jpg").expect("File open failed")
+            .decode().expect("File decode failed").into_rgb32f();
+        let mut data = Rgb32FImage::new(1000, 1000);
+        image::imageops::tile(&mut data, &image);
+        //println!("{:?}", data.dimensions());
+        let mut data_arr = Array::from_iter(data.iter().map(|x| *x)).into_shape((1, 1000, 1000, 3)).unwrap();
+        data_arr.swap_axes(1, 3);
+        data_arr.swap_axes(2, 3);
+        process_image(models, data_arr);
         println!("Not implemented");
     }
+    Ok(())
 }
 
 fn main(){
@@ -71,7 +88,7 @@ fn main(){
         .with_name("test")
         .with_log_level(LoggingLevel::Info)
         .build().unwrap();
-    let models = load_model_all(&environment);
+    let mut models = load_model_all(&environment);
 
-    process_image_all(&models, args[1..].to_vec());
+    process_image_all(&mut models, args[1..].to_vec());
 }
