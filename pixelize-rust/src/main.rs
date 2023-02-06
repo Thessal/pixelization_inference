d8aa395847158f6929a97aa2b710ddcb022e28d2
Jongkook Choi <jongkook90@gmail.com>
2023-02-06 17:53:15 +0900

    Added denoiser

--- pixelize-rust/src/main.rs
+++ pixelize-rust/src/main.rs
@@ -12,7 +12,7 @@ use std::collections::HashMap;
 use onnxruntime::{
     environment::Environment, 
     //ndarray::{Array, Dim, Ix0, Ix1, Ix2, Ix3, Ix4, Ix5, IxDyn, s, ArrayView},
-    ndarray::{Array, Ix2, Ix3, Ix4, IxDyn, s, },
+    ndarray::{Array, Ix2, Ix3, Ix4, Ix5, IxDyn, s, concatenate, Axis},
     //tensor::{OrtOwnedTensor, FromArray, InputTensor},
     //tensor::{OrtOwnedTensor},
     GraphOptimizationLevel, LoggingLevel, OrtError
@@ -30,6 +30,8 @@ fn load_model<'a>(environment:&'a Environment, bytes:&'a[u8]) -> Result<onnxrunt
 
 fn load_model_all<'a>(environment:&'a Environment)-> HashMap<std::string::String, onnxruntime::session::Session<'a>>{
     let mut model_data = HashMap::<String, &[u8]>::new();
+
+    // Pixelizer
     model_data.insert("alias_RGBEnc".to_string(), include_bytes!("../../alias_RGBEnc.onnx"));
     model_data.insert("alias_RGBDec".to_string(), include_bytes!("../../alias_RGBDec.onnx"));
     model_data.insert("g_a_RGBEnc".to_string(), include_bytes!("../../g_a_RGBEnc.onnx"));
@@ -37,17 +39,21 @@ fn load_model_all<'a>(environment:&'a Environment)-> HashMap<std::string::String
     model_data.insert("g_a_MLP".to_string(), include_bytes!("../../g_a_MLP.onnx"));
     model_data.insert("g_a_RGBDec_mod_conv_1".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_1.onnx"));
     model_data.insert("g_a_RGBDec_mod_conv_2".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_2.onnx"));
-    model_data.insert("g_a_RGBDec_mod_conv_3".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_3.onnx"));
-    model_data.insert("g_a_RGBDec_mod_conv_4".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_4.onnx"));
-    model_data.insert("g_a_RGBDec_mod_conv_5".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_5.onnx"));
-    model_data.insert("g_a_RGBDec_mod_conv_6".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_6.onnx"));
-    model_data.insert("g_a_RGBDec_mod_conv_7".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_7.onnx"));
-    model_data.insert("g_a_RGBDec_mod_conv_8".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_8.onnx"));
+    //model_data.insert("g_a_RGBDec_mod_conv_3".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_3.onnx"));
+    //model_data.insert("g_a_RGBDec_mod_conv_4".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_4.onnx"));
+    //model_data.insert("g_a_RGBDec_mod_conv_5".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_5.onnx"));
+    //model_data.insert("g_a_RGBDec_mod_conv_6".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_6.onnx"));
+    //model_data.insert("g_a_RGBDec_mod_conv_7".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_7.onnx"));
+    //model_data.insert("g_a_RGBDec_mod_conv_8".to_string(), include_bytes!("../../g_a_RGBDec_mod_conv_8.onnx"));
     model_data.insert("g_a_RGBDec_upsample_block1".to_string(), include_bytes!("../../g_a_RGBDec_upsample_block1.onnx"));
     model_data.insert("g_a_RGBDec_upsample_block2".to_string(), include_bytes!("../../g_a_RGBDec_upsample_block2.onnx"));
     model_data.insert("g_a_RGBDec_conv_1".to_string(), include_bytes!("../../g_a_RGBDec_conv_1.onnx"));
     model_data.insert("g_a_RGBDec_conv_2".to_string(), include_bytes!("../../g_a_RGBDec_conv_2.onnx"));
     model_data.insert("g_a_RGBDec_conv_3".to_string(), include_bytes!("../../g_a_RGBDec_conv_3.onnx"));
+
+    // FastDVDNet
+    model_data.insert("block1".to_string(), include_bytes!("../../block1.onnx"));
+    model_data.insert("block2".to_string(), include_bytes!("../../block2.onnx"));
 
     let model_names = model_data.keys().cloned();
     let models = model_names
@@ -89,7 +95,6 @@ fn normalize(data: Array<f32, Ix4>)->Array<f32, Ix4>{
 }
 
 
-
 fn g_a_RGBDec(models: &mut HashMap<String, onnxruntime::session::Session<'_>>, 
               x:Array<f32, Ix4>, code:Array<f32, Ix2>) -> Array<f32, Ix4>{
     let mut x_ : Array<f32, IxDyn> = x.clone().into_dyn();
@@ -113,7 +118,7 @@ fn g_a_RGBDec(models: &mut HashMap<String, onnxruntime::session::Session<'_>>,
         }
         x_ = x_ + residual;
     }
-    {
+    { // TODO : reduce redundancy
         let residual = x_.clone();
         {
             let g_a_RGBDec_mod_conv_2 = models.get_mut(&("g_a_RGBDec_mod_conv_2".to_string())).unwrap();
@@ -299,6 +304,43 @@ fn load_file(filename: String) -> Media{
 	};
 	media
 }
+
+fn denoise(models: &mut HashMap<String, onnxruntime::session::Session<'_>>,
+           frames:Array<f32, Ix5>, noise_level:f32) -> Array<f32, Ix4>{
+    let block1 = models.get_mut(&("block1".to_string())).unwrap();
+    let noise_arr : Array<f32, Ix4> = Array::ones((1,3,1000,1000)).mul(noise_level);
+    let noise_arr = noise_arr.into_dyn();
+
+    let a0 : Array<f32, Ix5> = frames.slice(s![..,0..3,..,..,..]).to_owned();
+    let a0 = a0.into_shape((1,9,1000,1000)).unwrap().into_dyn();
+    let a0:Array<f32, IxDyn> = concatenate![Axis(1), a0, noise_arr];
+    let b0:Array<f32, Ix4> = frames.slice(s![..,1,..,..,..]).to_owned();
+    let b0 = b0.into_dyn();
+    let c0 = (*block1.run::<'_, '_, '_, f32, f32, IxDyn>(vec![a0, b0]).unwrap()[0]).to_owned();
+
+    let a1 : Array<f32, Ix5> = frames.slice(s![..,1..4,..,..,..]).to_owned();
+    let a1 = a1.into_shape((1,9,1000,1000)).unwrap().into_dyn();
+    let a1:Array<f32, IxDyn> = concatenate![Axis(1), a1, noise_arr];
+    let b1:Array<f32, Ix4> = frames.slice(s![..,1,..,..,..]).to_owned();
+    let b1 = b1.into_dyn();
+    let c1 = (*block1.run::<'_, '_, '_, f32, f32, IxDyn>(vec![a1, b1]).unwrap()[0]).to_owned();
+
+    let a2 : Array<f32, Ix5> = frames.slice(s![..,2..5,..,..,..]).to_owned();
+    let a2 = a2.into_shape((1,9,1000,1000)).unwrap().into_dyn();
+    let a2:Array<f32, IxDyn> = concatenate![Axis(1), a2, noise_arr];
+    let b2:Array<f32, Ix4> = frames.slice(s![..,1,..,..,..]).to_owned();
+    let b2 = b2.into_dyn();
+    let c2 = (*block1.run::<'_, '_, '_, f32, f32, IxDyn>(vec![a2, b2]).unwrap()[0]).to_owned();
+
+    let block2 = models.get_mut(&("block2".to_string())).unwrap();
+    let d:Array<f32, IxDyn> = concatenate![Axis(1), c0, c1, c2, noise_arr];
+    let e:Array<f32, IxDyn> = frames.slice(s![..,2,..,..,..]).to_owned().into_dyn();
+    let result = (*block2.run::<'_, '_, '_, f32, f32, IxDyn>(vec![d, e]).unwrap()[0]).to_owned();
+
+    let output: Array<f32, Ix4> = result.into_dimensionality::<Ix4>().unwrap();
+    output
+}
+
 
 fn process_image_all(models: &mut HashMap<String, onnxruntime::session::Session<'_>>, image_path: Vec<String>) -> Result<(),()>{
     let reference_b = Cursor::new(include_bytes!("../../reference.png"));
